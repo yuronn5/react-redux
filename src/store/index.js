d3d64effffad17659b35abb940830c2ad3857e0e
yurii bilous <yurii.b@elogic.com.ua>
2024-09-14 16:49:22 +0200

    for chrome redux extension

--- src/store/index.js
+++ src/store/index.js
@@ -7,4 +7,4 @@ const rootReducer = combineReducers({
     customers: customerReducer
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
