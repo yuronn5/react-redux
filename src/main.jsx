a1e40f8a8c5738a54cb623b2c0d159e6d74f645d
yurii bilous <yurii.b@elogic.com.ua>
2024-09-14 21:52:16 +0200

    refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { store } from "./store/index.js";
 
