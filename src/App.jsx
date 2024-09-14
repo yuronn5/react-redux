32d5b8b8461ba304fa3c31f9d12f663f73620243
yurii bilous <yurii.b@elogic.com.ua>
2024-09-14 16:34:45 +0200

    simple redux functionality, created reducer and actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import './App.css'
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <div>{cash}</div>
+      <div>
+        <button onClick={() => addCash(Number(prompt()))}>Add Cash</button>
+        <button onClick={() => getCash(Number(prompt()))}>Get Cash</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const defaultState = {
+  cash: 0,
+}
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'ADD_CASH':
+      return {...state, cash: state.cash + action.payload}
+    case 'GET_CASH':
+      return {...state, cash: state.cash - action.payload}
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+       <App />
+    </Provider>
   </StrictMode>,
 )
